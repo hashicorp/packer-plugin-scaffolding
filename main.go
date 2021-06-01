510d679c9093cab79182ca9755890915c7ac253c
Ambrose Chua <ambrose@hey.com>
2021-06-01 16:13:56 +0800

    Move version to its own package (#47)

--- main.go
+++ main.go
@@ -7,23 +7,9 @@ import (
 	scaffoldingData "packer-plugin-scaffolding/datasource/scaffolding"
 	scaffoldingPP "packer-plugin-scaffolding/post-processor/scaffolding"
 	scaffoldingProv "packer-plugin-scaffolding/provisioner/scaffolding"
+	scaffoldingVersion "packer-plugin-scaffolding/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "0.0.1"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
 )
 
 func main() {
@@ -32,7 +18,7 @@ func main() {
 	pps.RegisterProvisioner("my-provisioner", new(scaffoldingProv.Provisioner))
 	pps.RegisterPostProcessor("my-post-processor", new(scaffoldingPP.PostProcessor))
 	pps.RegisterDatasource("my-datasource", new(scaffoldingData.Datasource))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(scaffoldingVersion.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
